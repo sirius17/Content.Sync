3d76a9fd8e74a4f48fb7e82311546d48020c4942
Nikhil Prasad <nikhil.prasad@gmail.com>
2013-09-02 18:09:56 +0530

    Added Sample command for testing.

---
+++ Content.Sync.Clarifi/SampleCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Sync.Clarifi
+{
+    public class SampleCommand : HotelUpdateCommand
+    {
+        protected override Task ProcessHotelWorkItem(HotelWorkItem workItem, System.Threading.CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
