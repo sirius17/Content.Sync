5a2647f443ea86434012d4589c291e4318f8a761
Mahesh Udhavrao Tokle <mtokle@upcurve.in>
2013-09-05 14:39:21 +0530

    Update commands added : Image, InOutDetails and NearByAttractions

--- Content.Sync.UpdateCommands/HotelUpdate.cs
+++ Content.Sync.UpdateCommands/HotelUpdate.cs
@@ -6,20 +6,36 @@ using System.Threading.Tasks;
 using Content.Sync.Clarifi;
 using Content.Sync.Data.Factories;
 using ContentUpdate.Hotel.Core.Interfaces;
+using ContentUpdate.Hotel.Model;
 
 namespace Content.Sync.UpdateCommands
 {
     public class HotelUpdate : HotelUpdateCommand
     {
         private readonly IDatabaseManager _appacitive = AppacitiveDataProviderFactory.GetDatabaseManager();
-        private readonly IDatabaseManager _sqlDb = AppacitiveDataProviderFactory.GetDatabaseManager();
+        private readonly IDatabaseManager _sqlDb = SqlDataProviderFactory.GetDatabaseManager();
         protected override Task ProcessHotelWorkItem(WorkItem workItem, System.Threading.CancellationToken cancellationToken)
         {
-            //1. Get the hotel article from the appacitive
-            
-            //2. Get the hotel row from the client database
-            //3. If hotel not found in the database then insert hotel else update the hotel data.
-            throw new NotImplementedException();
+            var key = new HotelProperty()
+            {
+                AppacitiveId = workItem.HotelArticleId,
+                PropertyId = long.Parse(workItem.HotelId),
+                SupplierFamily = workItem.SupplierFamily
+            };
+            if (workItem.DataType != "Deleted")
+            {
+                //1. Get the hotel article from the appacitive
+                HotelProperty changedHotel = _appacitive.GetPrimaryContent(_appacitive.CreateHotelKey(key));
+
+                //2. If hotel not found in the database then insert hotel else update the hotel data.
+                _sqlDb.SaveHotelAndDetails(changedHotel);
+            }
+            else
+            {
+                //Disable hotel
+                _sqlDb.DisableHotel(_sqlDb.CreateHotelKey(key));
+            }
+            return null;
         }
 
         protected override Task OnFault(WorkItem workItem, Exception ex)
--- Content.Sync.UpdateCommands/InOutDetailsUpdate.cs
+++ Content.Sync.UpdateCommands/InOutDetailsUpdate.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Content.Sync.Clarifi;
+using Content.Sync.Data.Factories;
+using ContentUpdate.Hotel.Core.Interfaces;
+using ContentUpdate.Hotel.Model;
 
 namespace Content.Sync.UpdateCommands
 {
     public class InOutDetailsUpdate : HotelUpdateCommand
     {
+        private readonly IDatabaseManager _appacitive = AppacitiveDataProviderFactory.GetDatabaseManager();
+        private readonly IDatabaseManager _sqlDb = SqlDataProviderFactory.GetDatabaseManager();
+
         protected override Task ProcessHotelWorkItem(WorkItem workItem, System.Threading.CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var key = new HotelProperty()
+            {
+                AppacitiveId = workItem.HotelArticleId,
+                PropertyId = long.Parse(workItem.HotelId),
+                SupplierFamily = workItem.SupplierFamily
+            };
+            var sqlHotelKey = _sqlDb.CreateHotelKey(key);
+            var appacitiveHotelKey = _appacitive.CreateHotelKey(key);
+
+            //1. Get the checkinout details from the appacitive
+            List<HotelTimeDetail> inDetails = null;
+            List<HotelTimeDetail> outDetails = null;
+            _appacitive.GetCheckInOutDetails(appacitiveHotelKey, out inDetails, out outDetails);
+            
+            //2. Update/Add the checkinout details
+            _sqlDb.SaveHotelInOutDetails(sqlHotelKey, inDetails, outDetails);
+            return null;
         }
 
         protected override Task OnFault(WorkItem workItem, Exception ex)
