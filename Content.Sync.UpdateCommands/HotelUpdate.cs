87e454209be2353ea2bb20ce47fd547ddac6f4dc
Mahesh Udhavrao Tokle <mtokle@upcurve.in>
2013-09-05 09:19:10 +0530

    Clarifi Model, DALs dlls added

--- Content.Sync.UpdateCommands/HotelUpdate.cs
+++ Content.Sync.UpdateCommands/HotelUpdate.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Content.Sync.Clarifi;
+using Content.Sync.Data.Factories;
+using ContentUpdate.Hotel.Core.Interfaces;
 
 namespace Content.Sync.UpdateCommands
 {
     public class HotelUpdate : HotelUpdateCommand
     {
+        private readonly IDatabaseManager _appacitive = AppacitiveDataProviderFactory.GetDatabaseManager();
+        private readonly IDatabaseManager _sqlDb = AppacitiveDataProviderFactory.GetDatabaseManager();
         protected override Task ProcessHotelWorkItem(WorkItem workItem, System.Threading.CancellationToken cancellationToken)
         {
+            //1. Get the hotel article from the appacitive
+            
+            //2. Get the hotel row from the client database
+            //3. If hotel not found in the database then insert hotel else update the hotel data.
             throw new NotImplementedException();
         }
 
